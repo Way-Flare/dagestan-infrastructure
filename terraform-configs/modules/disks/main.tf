c9233d63c186ff21e1d1b3e89fc16ade153f48bc
etozhefifan <coofi2012@gmail.com>
2024-05-14 07:59:05 +0300

    added new terraform configs

---
+++ terraform-configs/modules/disks/main.tf
@@ -0,0 +1,23 @@
+resource "yandex_compute_disk" "boot-disk-for-test-stage" {
+  name     = "boot-disk-for-test-stage"
+  type     = "network-hdd"
+  zone     = "ru-central1-b"
+  size     = "30"
+  image_id = "fd89n8278rhueakslujo"
+}
+
+resource "yandex_compute_disk" "boot-disk-for-balancer" {
+  name     = "boot-disk-for-balancer"
+  type     = "network-hdd"
+  zone     = "ru-central1-b"
+  size     = "20"
+  image_id = "fd89n8278rhueakslujo"
+}
+
+resource "yandex_compute_disk" "boot-disk-for-monitoring" {
+  name     = "boot-disk-for-monitoring"
+  type     = "network-hdd"
+  zone     = "ru-central1-b"
+  size     = "20"
+  image_id = "fd89n8278rhueakslujo"
+}
---
+++ terraform-configs/modules/disks/outputs.tf
@@ -0,0 +1,12 @@
+output "disk-test-stage" {
+  value = yandex_compute_disk.boot-disk-for-test-stage.id
+}
+
+output "disk-balancer" {
+  value = yandex_compute_disk.boot-disk-for-balancer.id
+}
+
+output "disk-monitoring" {
+  value = yandex_compute_disk.boot-disk-for-monitoring.id
+}
+
